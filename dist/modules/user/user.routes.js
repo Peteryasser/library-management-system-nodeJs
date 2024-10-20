61a26728727a63c3025e46cd684ec21f75683232
Peteryasser <96436582+Peteryasser@users.noreply.github.com>
2024-10-20 17:34:07 +0300

    Delete dist directory

--- dist/modules/user/user.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controller_1 = require("./user.controller");
-const router = (0, express_1.Router)();
-const userController = new user_controller_1.UserController();
-router.post('/register', (req, res, next) => userController.register(req, res, next));
-router.post('/login', (req, res, next) => userController.login(req, res, next));
-exports.default = router;
