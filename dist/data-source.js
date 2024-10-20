07b2d493900cc7ea9b70b7131737a62ec84fbbad
Peteryasser <96436582+Peteryasser@users.noreply.github.com>
2024-10-20 17:35:45 +0300

    Update data file

--- dist/data-source.js
+++ dist/data-source.js
@@ -12,8 +12,8 @@ exports.AppDataSource = new typeorm_1.DataSource({
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+    entities: [__dirname + '/../entities/*.js'],
+    migrations: [__dirname + '/../migrations/*.js'],
     synchronize: false,
     logging: false
 });
